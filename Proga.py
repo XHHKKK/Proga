3b33ccb273b00297eba00c02b2eec9e328016616
XHHKKK <tarelkaizpodolivok@gmail.com>
2025-04-03 17:22:52 +0300

    Два списка

--- Proga.py
+++ Proga.py
@@ -2,5 +2,13 @@ from tkinter import *
 window = Tk()
 window.title( 'AddressApp' )
 window.resizable(0,0)
+Label(text="Введите номер станции назначения:").grid(row=0, column=0, sticky=N+W,
+                                                     pady=10, padx=10)
+Scrollbar(window).grid(row=0, column=2, sticky=N+S+E+W)
+station_numb = Entry(width = 3)
+Listbox(window, width = 50).grid(row=0, column=1)
+station_list = Listbox(window, width = 50)
+station_list.grid(row=0, column=2, columnspan = 5,
+                  padx=10)
 
 window.mainloop()
